bbbef167802e79a2cdf9799357a29c9f20545daf
David Adametz <20501597+david-adametz@users.noreply.github.com>
2024-08-24 18:01:53 +0200

    Rename yaml.py to yml.py.

--- rwkit/__init__.py
+++ rwkit/__init__.py
@@ -4,7 +4,7 @@ rwkit
 
 from .json import read_json, read_jsonl, write_json, write_jsonl
 from .text import read_lines, read_text, write_lines, write_text
-from .yaml import read_yaml, write_yaml
+from .yml import read_yaml, write_yaml
 
 __all__ = [
     "read_json",
