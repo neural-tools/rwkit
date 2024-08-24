d5fe3fce7c3196ef7226b99da0ee6b2dd7ad352b
David Adametz <20501597+david-adametz@users.noreply.github.com>
2024-08-24 17:39:46 +0200

    Drop docx support.

--- rwkit/__init__.py
+++ rwkit/__init__.py
@@ -2,19 +2,16 @@
 rwkit
 """
 
-from .docx import read_docx, write_docx
 from .json import read_json, read_jsonl, write_json, write_jsonl
 from .text import read_lines, read_text, write_lines, write_text
 from .yaml import read_yaml, write_yaml
 
 __all__ = [
-    "read_docx",
     "read_json",
     "read_jsonl",
     "read_lines",
     "read_text",
     "read_yaml",
-    "write_docx",
     "write_json",
     "write_jsonl",
     "write_lines",
