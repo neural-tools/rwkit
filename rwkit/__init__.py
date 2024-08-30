c8712f7aed5b01c9b50b608db7e0c16777726a0a
David Adametz <20501597+david-adametz@users.noreply.github.com>
2024-08-30 22:02:18 +0200

    Enable __version__ on import.

--- rwkit/__init__.py
+++ rwkit/__init__.py
@@ -2,9 +2,17 @@
 rwkit
 """
 
+from importlib.metadata import PackageNotFoundError, version
+
 from .io_json import read_json, read_jsonl, write_json, write_jsonl
 from .io_text import read_lines, read_text, write_lines, write_text
 from .io_yaml import read_yaml, write_yaml
+
+try:
+    __version__ = version("rwkit")
+except PackageNotFoundError:
+    # Package is not installed
+    __version__ = "unknown"
 
 __all__ = [
     "read_text",
